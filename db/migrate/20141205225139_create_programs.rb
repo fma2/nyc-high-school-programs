31835f30368aefa8a2ec497e54bb8c5429237e22
Farheen Malik <farheen.m.malik@gmail.com>
2014-12-05 18:41:42 -0500

    Create migration for new model programs

---
+++ db/migrate/20141205225139_create_programs.rb
@@ -0,0 +1,17 @@
+class CreatePrograms < ActiveRecord::Migration
+  def change
+    create_table :programs do |t|
+    	t.string :program_code
+    	t.string :program_name
+    	t.string :dbn
+    	t.string :printed_school_name
+    	t.string :interest_area
+    	t.string :selection_method
+    	t.string :selection_method_abbrevi
+    	t.string :directory_page_
+    	t.string :borough
+    	t.string :urls
+      t.timestamps
+    end
+  end
+end
