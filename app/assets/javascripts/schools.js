e1bafcac5978d26f35b49f65df90a2441c83d19e
Farheen Malik <farheen.m.malik@gmail.com>
2014-12-05 21:16:57 -0500

    Modify drawing to accomodate new database structure - only pulls from schools table

--- app/assets/javascripts/schools.js
+++ app/assets/javascripts/schools.js
@@ -10,7 +10,7 @@ function prepareDataforMap(schoolData) {
   var schoolDataAry = new Array();
   for (i = 0; i < schoolData.length; i++) {
     schoolDataAry.push(
-      [schoolData[i].printed_school_name, 
+      [schoolData[i].school_name, 
       parseFloat(schoolData[i].latitude), 
       parseFloat(schoolData[i].longitude), 
       schoolData[i].primary_address_line_1, 
@@ -18,15 +18,16 @@ function prepareDataforMap(schoolData) {
       schoolData[i].state_code, 
       schoolData[i].zip, 
       schoolData[i].school_type, 
-      schoolData[i].interest_area, 
-      schoolData[i].program_name, 
-      schoolData[i].urls
+      schoolData[i].total_students,
+      schoolData[i].program_highlights,
+      schoolData[i].overview_paragraph,
+      schoolData[i].extracurricular_activities,
+      schoolData[i].website
       ])
   };
   return schoolDataAry;
 };
-  
-function addMarker(school_name, lat, lng, address, city, state, zip, school_type, interest_area, program_name, urls, map) {
+function addMarker(school_name, lat, lng, address, city, state, zip, school_type, total_students, program_highlights, overview_paragraph, extracurricular_activities, website, map) {
   var point = new google.maps.LatLng(lat, lng);
   var marker = new google.maps.Marker({
       position: point,
@@ -42,13 +43,13 @@ function addMarker(school_name, lat, lng, address, city, state, zip, school_type
       '<div id="bodyContent">'+
       '<p><b>Address: </b>' + address + ', ' + city + ', ' + state + ' ' + zip + '<br>' +
       '<b>Type of School: </b>' + school_type + '</p>' +
-
-      '<p><b>Program Name: </b>' + program_name + '<br>' +
-      '<b>Interest Area: </b>' + interest_area + '<br>' +
-      '<b>Website: <a href=' + urls + '>'+ urls + '</a><br><br>' + 
+      '<p><b>Overview: </b>' + overview_paragraph + '<br>' +
+      '<b>Program Highlights: </b>' + program_highlights + '<br>' +
+      '<b>Extracurricular Activities: </b>' + extracurricular_activities+ '<br>' +
+      '<b>Website: <a href=' + website + '>'+ website + '</a><br><br>' + 
       '</div>'+
       '</div>';
-      
+
   var infowindow = new google.maps.InfoWindow({ content: contentString });
   google.maps.event.addListener(marker, 'click', function() {
       infowindow.setContent(contentString);
@@ -93,11 +94,11 @@ function initialize() {
   // addMarker("Yo", 40.8, -73.97606602099967, map);
   // addMarker("Hi", 40.723, -73.97606602099967, map);
 
-
   var schoolData = prepareDataforMap(gon.schools);
   for (i = 0; i < schoolData.length; i++) {
-    addMarker(schoolData[i][0], schoolData[i][1], schoolData[i][2], schoolData[i][3],schoolData[i][4],schoolData[i][5], schoolData[i][6], schoolData[i][7], schoolData[i][8], schoolData[i][9], schoolData[i][10], map)
+    addMarker(schoolData[i][0], schoolData[i][1], schoolData[i][2], schoolData[i][3],schoolData[i][4],schoolData[i][5], schoolData[i][6], schoolData[i][7], schoolData[i][8], schoolData[i][9], schoolData[i][10], schoolData[i][11], schoolData[i][12], map)
   }
+
   // Marker Clusterer code
   // var schoolData= prepareDataforMap(gon.schools);
   // var markers = [];
