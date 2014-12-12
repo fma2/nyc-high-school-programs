46329c17ff8ede9966670a8c3e14dbaf17a50192
Farheen Malik <farheen.m.malik@gmail.com>
2014-12-12 15:09:02 -0500

    Modify index method for mapbox.
    Comment out gon.schools.  Create geojson from schools database.  Create respond_to method to return json for ajax call for the map.

--- app/controllers/schools_controller.rb
+++ app/controllers/schools_controller.rb
@@ -1,7 +1,28 @@
 class SchoolsController < ApplicationController
 	# skip_before_action :verify_authenticity_token
 	def index
-		gon.schools = School.all
+		# gon.schools = School.all
+		@schools = School.all
+		@geojson = Array.new
+		@schools.each do |school|
+			@geojson << {
+				type: 'Feature',
+				geometry: { 
+					type: 'Point',
+					coordinates: [school.longitude, school.latitude]
+				},
+				properties: {
+					name: school.school_name,
+					address: school.primary_address_line_1,
+					:'marker-color' => '#00607d',
+      		:'marker-symbol' => 'circle',
+      		:'marker-size' => 'medium'
+				}
+			}
+		end
+		respond_to do |format|
+			format.html
+			format.json { render json: @geojson }
+		end
 	end
-
 end
