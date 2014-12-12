fb21f6d9a5eecdf5d53d45db1c7ba7d055e4ef25
Farheen Malik <farheen.m.malik@gmail.com>
2014-12-12 15:10:26 -0500

    Change root route back to schools index

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
-  root "welcome#index"
-  get '/program_search' => 'programs#search'
-  get '/results_map' => 'programs#map_results'
+  root "schools#index"
+  # get '/program_search' => 'programs#search'
+  # get '/results_map' => 'programs#map_results'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
