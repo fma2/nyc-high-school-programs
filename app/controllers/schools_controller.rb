affa56e9760d6ea5cec9adab6170b9090bfd7ba8
Farheen Malik <farheen.m.malik@gmail.com>
2014-12-13 15:26:07 -0500

    Add additional properties to school so can be used for map

--- app/controllers/schools_controller.rb
+++ app/controllers/schools_controller.rb
@@ -1,7 +1,5 @@
 class SchoolsController < ApplicationController
-	# skip_before_action :verify_authenticity_token
 	def index
-		# gon.schools = School.all
 		@schools = School.all
 		@geojson = Array.new
 		@schools.each do |school|
@@ -14,6 +12,16 @@ class SchoolsController < ApplicationController
 				properties: {
 					name: school.school_name,
 					address: school.primary_address_line_1,
+					zip: school.zip,
+					boro: school.boro,
+					type: school.school_type,
+					total_students: school.total_students,
+					program_highlights: school.program_highlights,
+					overview_paragraph: school.overview_paragraph,
+					website: school.website,
+					dbn: school.dbn,
+					grade_span_min: school.grade_span_min,
+					grade_span_max: school.grade_span_max,
 					:'marker-color' => '#00607d',
       		:'marker-symbol' => 'circle',
       		:'marker-size' => 'medium'
