e778fee0e869424e6d84dfd4329ac49a164d9752
Farheen Malik <farheen.m.malik@gmail.com>
2014-12-13 13:45:14 -0500

    Add MarkerClusterer code (not working yet) and add filter sidebar to filter types of schools - needs to be refactored

--- app/assets/javascripts/schools.js
+++ app/assets/javascripts/schools.js
@@ -1,20 +1,17 @@
 //Mapbox access token
 L.mapbox.accessToken = 'pk.eyJ1IjoiZm1hMiIsImEiOiJkcmdtd0NjIn0.dw0I__cIjfXpz37Yj0DQmw';
 
+
 //Place map
-var map = L.mapbox.map('map-canvas', 'examples.map-i86nkdio')
-    .setView([40.718961617000446,-73.97606602099967], 9);
+var map = L.mapbox.map('map', 'examples.map-zr0njcqy').setView([40.78,-73.94], 11);
 
 //Place markers
 $(document).ready(function() {
-  console.log("ready!");
   $.ajax({
     dataType: 'json',
     url: '/',
     type: 'GET'
   }).success(function(data) {  
-      console.log("success!");
-      console.log(data);
       map.featureLayer.setGeoJSON(data);
     })
   });
@@ -25,10 +22,194 @@ map.featureLayer.on('layeradd', function(e) {
   properties = marker.feature.properties;
   popupContent = '<div class="popup">' + 
                   '<h3>' + properties.name + '</h3>' +
-                  '<p>' + properties.address + '</p>' +
+                  '<p>' + properties.address + ', ' + properties.zip + '</p>' +
+                  '<p>Grades: '+ properties.grade_span_min + ' to ' + properties.grade_span_max + '</p>'
+                     '<p>'+'</p>'
+                  '<p>'+'</p>'
+                  '<p>'+'</p>'
+
                   '</div>'
   marker.bindPopup(popupContent, {
     closeButton: false, 
     minWidth: 320
   });
 })
+
+//Add MarkerClusterer - sample code
+L.mapbox.featureLayer('examples.map-h61e8o8e').on('ready', function(e) {
+  var clusterGroup = new L.MarkerClusterGroup();
+    e.target.eachLayer(function(layer) {
+        clusterGroup.addLayer(layer);
+    });
+    map.addLayer(clusterGroup);
+})
+
+
+//Filter sidebar
+var newSchools = document.getElementById('filter-new-schools');
+var consortiumSchools = document.getElementById('filter-consortium-schools');
+var cteSchools = document.getElementById('filter-cte-schools');
+var allGirlsSchools = document.getElementById('filter-all-girls-schools');
+var internationalSchools = document.getElementById('filter-international-schools');
+var specializedSchools = document.getElementById('filter-specialized-schools');
+var nycPTechSchools = document.getElementById('filter-p-tech-schools');
+var allBoysSchools = document.getElementById('filter-all-boys-schools');
+var all = document.getElementById('filter-all');
+
+newSchools.onclick = function(e) {
+  consortiumSchools.className = '';
+  cteSchools.className = '';
+  allGirlsSchools.className = '';
+  internationalSchools.className = '';
+  specializedSchools.className = '';
+  nycPTechSchools.className = '';
+  allBoysSchools.className = '';
+  all.className = '';
+
+  this.className = 'active';
+  // The setFilter function takes a GeoJSON feature object
+  // and returns true to show it or false to hide it.
+  map.featureLayer.setFilter(function(f) {
+    return f.properties['type'] === 'New School';
+  });
+  return false;
+};
+
+consortiumSchools.onclick = function(e) {
+  newSchools.className = '';
+  cteSchools.className = '';
+  allGirlsSchools.className = '';
+  internationalSchools.className = '';
+  specializedSchools.className = '';
+  nycPTechSchools.className = '';
+  allBoysSchools.className = '';
+  all.className = '';
+  
+  this.className = 'active';
+  // The setFilter function takes a GeoJSON feature object
+  // and returns true to show it or false to hide it.
+  map.featureLayer.setFilter(function(f) {
+    return f.properties['type'] === 'Consortium School';
+  });
+  return false;
+}
+
+cteSchools.onclick = function(e) {
+  newSchools.className = '';
+  consortiumSchools.className = '';
+  allGirlsSchools.className = '';
+  internationalSchools.className = '';
+  specializedSchools.className = '';
+  nycPTechSchools.className = '';
+  allBoysSchools.className = '';
+  all.className = '';
+  
+  this.className = 'active';
+  map.featureLayer.setFilter(function(f) {
+    return f.properties['type'] === 'CTE School';
+  });
+  return false;
+}
+
+allGirlsSchools.onclick = function(e) {
+  newSchools.className = '';
+  consortiumSchools.className = '';
+  cteSchools.className = '';
+  internationalSchools.className = '';
+  specializedSchools.className = '';
+  nycPTechSchools.className = '';
+  allBoysSchools.className = '';
+  all.className = '';
+  
+  this.className = 'active';
+  map.featureLayer.setFilter(function(f) {
+    return f.properties['type'] === 'All-Girls School';
+  });
+  return false;
+}
+
+internationalSchools.onclick = function(e) {
+  newSchools.className = '';
+  consortiumSchools.className = '';
+  cteSchools.className = '';
+  allGirlsSchools.className = '';
+  specializedSchools.className = '';
+  nycPTechSchools.className = '';
+  allBoysSchools.className = '';
+  all.className = '';
+  
+  this.className = 'active';
+  map.featureLayer.setFilter(function(f) {
+    return f.properties['type'] === 'International School';
+  });
+  return false;
+}
+
+specializedSchools.onclick = function(e) {
+  newSchools.className = '';
+  consortiumSchools.className = '';
+  cteSchools.className = '';
+  allGirlsSchools.className = '';
+  internationalSchools.className = '';
+  nycPTechSchools.className = '';
+  allBoysSchools.className = '';
+  all.className = '';
+  
+  this.className = 'active';
+  map.featureLayer.setFilter(function(f) {
+    return f.properties['type'] === 'Specialized School';
+  });
+  return false;
+}
+
+nycPTechSchools.onclick = function(e) {
+  newSchools.className = '';
+  consortiumSchools.className = '';
+  cteSchools.className = '';
+  allGirlsSchools.className = '';
+  internationalSchools.className = '';
+  specializedSchools.className = '';
+  allBoysSchools.className = '';
+  all.className = '';
+  
+  this.className = 'active';
+  map.featureLayer.setFilter(function(f) {
+    return f.properties['type'] === 'NYC P-Tech 9-14, New School';
+  });
+  return false;
+}
+
+
+allBoysSchools.onclick = function(e) {
+  newSchools.className = '';
+  consortiumSchools.className = '';
+  cteSchools.className = '';
+  allGirlsSchools.className = '';
+  internationalSchools.className = '';
+  specializedSchools.className = '';
+  nycPTechSchools.className = '';
+  all.className = '';
+  
+  this.className = 'active';
+  map.featureLayer.setFilter(function(f) {
+    return f.properties['type'] === 'All-Boys School';
+  });
+  return false;
+}
+
+all.onclick = function() {
+  newSchools.className = '';
+  consortiumSchools.className = '';
+  cteSchools.className = '';
+  allGirlsSchools.className = '';
+  internationalSchools.className = '';
+  specializedSchools.className = '';
+  nycPTechSchools.className = '';
+  allBoysSchools = '';
+  this.className = 'active';
+  map.featureLayer.setFilter(function(f) {
+      // Returning true for all markers shows everything.
+      return true;
+  });
+  return false;
+};
