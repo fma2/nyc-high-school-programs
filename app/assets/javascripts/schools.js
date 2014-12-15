37777c9100173d6af6f7b7347bab28abd7a1a9be
Farheen Malik <farheen.m.malik@gmail.com>
2014-12-14 22:49:36 -0500

    Add method to filter markers in markerclusterer with checkbox on school type

--- app/assets/javascripts/schools.js
+++ app/assets/javascripts/schools.js
@@ -2,22 +2,21 @@
 L.mapbox.accessToken = 'pk.eyJ1IjoiZm1hMiIsImEiOiJkcmdtd0NjIn0.dw0I__cIjfXpz37Yj0DQmw';
 
 var loader = document.getElementById('loader');
-
-//Place map
-var map = L.mapbox.map('map').setView([40.78, -73.94], 11).addLayer(L.mapbox.tileLayer('fma2.keb4h838'));
-
-startLoading();
-
 function startLoading() {
   loader.className = '';
 }
-
 function finishedLoading() {
   loader.className = 'done';
   setTimeout(function() {
     loader.className = 'hide';
   }, 1000);
 }
+//Place map
+var map = L.mapbox.map('map').setView([40.78, -73.94], 11).addLayer(L.mapbox.tileLayer('fma2.keb4h838'));
+
+startLoading();
+
+var filters = document.getElementById('filters');
 
 $(document).ready(function() {
   $.ajax({
@@ -26,25 +25,97 @@ $(document).ready(function() {
     type: 'GET'
   }).success(function(data) {
     var featureLayer = L.mapbox.featureLayer(data)
-    var clusterGroup = new L.MarkerClusterGroup({});
+    var clusterGroup = new L.MarkerClusterGroup();
     featureLayer.eachLayer(function(layer) {
       clusterGroup.addLayer(layer);
     })
     map.addLayer(clusterGroup);
+
+    var typesObj = {}, types = [];
+
     clusterGroup.eachLayer(function(marker) {
       var properties = marker.feature.properties
       popupContent = '<div class="popup">' +
       '<h3>' + properties.name + '</h3>' +
       '<p>' + properties.address + ', ' + properties.zip + '</p>' +
       '<p>Grades: ' + properties.grade_span_min + ' to ' + properties.grade_span_max + '</p>' +
+      '<p>Type: ' + properties.type + '</p>' +
       '</div>'
       marker.bindPopup(popupContent, {
         closeButton: false,
         minWidth: 320
       });
+      var feature = marker.feature
+      typesObj[feature.properties['type']] = true;
     })
+    for (var k in typesObj) types.push(k);
+    var checkboxes = [];
+    for (var i = 0; i < types.length; i++) {
+    // Create an an input checkbox and label inside.
+      var item = filters.appendChild(document.createElement('div'));
+      var checkbox = item.appendChild(document.createElement('input'));
+      var label = item.appendChild(document.createElement('label'));
+      checkbox.type = 'checkbox';
+      checkbox.id = types[i];
+      checkbox.checked = true;
+      // create a label to the right of the checkbox with explanatory text
+      label.innerHTML = types[i];
+      label.setAttribute('for', types[i]);
+      // Whenever a person clicks on this checkbox, call the update().
+      checkbox.addEventListener('change', update);
+      checkboxes.push(checkbox);
+    }
+     // This function is called whenever someone clicks on a checkbox and changes
+  // the selection of markers to be displayed.
+    function update() {
+      console.log("update pressed")
+      var enabled = {};
+      // Run through each checkbox and record whether it is checked. If it is,
+      // add it to the object of types to display, otherwise do not.
+      for (var i = 0; i < checkboxes.length; i++) {
+        console.log("checking if checked")
+        if (checkboxes[i].checked) enabled[checkboxes[i].id] = true;
+        console.log(checkboxes);
+
+      }
+      featureLayer.setFilter(function(feature) {
+        // console.log(feature);
+        var x = (feature.properties['type'] in enabled)
+        console.log(enabled);
+        // console.log("trying to filter");
+      // If this symbol is in the list, return true. if not, return false.
+      // The 'in' operator in javascript does exactly that: given a string
+      // or number, it says if that is in a object.
+      // 2 in { 2: true } // true
+      // 2 in { } // false
+        return (feature.properties['type'] in enabled);
+      });
+
+      map.removeLayer(clusterGroup);
+      clusterGroup = new L.MarkerClusterGroup();
+      featureLayer.eachLayer(function(layer) {
+        clusterGroup.addLayer(layer);
+      })
+      map.addLayer(clusterGroup);
+      clusterGroup.eachLayer(function(marker) {
+        var properties = marker.feature.properties
+        popupContent = '<div class="popup">' +
+                      '<h3>' + properties.name + '</h3>' +
+                      '<p>' + properties.address + ', ' + properties.zip + '</p>' +
+                      '<p>Grades: ' + properties.grade_span_min + ' to ' + properties.grade_span_max + '</p>' +
+                      '<p>Type: ' + properties.type + '</p>' +
+                      '</div>'
+        marker.bindPopup(popupContent, {
+          closeButton: false,
+          minWidth: 320
+        });
+        var feature = marker.feature
+        typesObj[feature.properties['type']] = true;
+      })
+    }
   })
 })
+
 
 // // Place markers
 // $(document).ready(function() {
