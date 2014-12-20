071bc8c4ce81ddf1d54e9e735de27386611e07f4
Farheen Malik <farheen.m.malik@gmail.com>
2014-12-20 15:33:18 -0500

    Move programs button on click method to programs.js from schools.js

---
+++ app/assets/javascripts/programs.js
@@ -0,0 +1,30 @@
+var programsButton = document.getElementById('programs');
+programsButton.onclick = function(e) {
+  $.ajax({
+    dataType: 'json',
+    url: '/programs',
+    type: 'GET'
+  }).success(function(data) {
+    var featureLayer = L.mapbox.featureLayer(data)
+    var clusterGroup = new L.MarkerClusterGroup();
+    featureLayer.eachLayer(function(layer) {
+      clusterGroup.addLayer(layer);
+    })
+    map.addLayer(clusterGroup);
+    clusterGroup.eachLayer(function(marker) {
+      var properties = marker.feature.properties
+      popupContent = '<div class="popup">' +
+      '<h3>' + properties.name + '</h3>' +
+      '<p>' + properties.address + ', ' + properties.zip + '</p>' +
+      '<p>Grades: ' + properties.grade_span_min + ' to ' + properties.grade_span_max + '</p>' +
+      '<p>Type: ' + properties.type + '</p>' +
+      '<p>Program Name: ' + properties.program_name + '</p>' +
+      '</div>'
+      marker.bindPopup(popupContent, {
+        closeButton: false,
+        minWidth: 320
+      });
+      var feature = marker.feature
+    })
+  });
+}
