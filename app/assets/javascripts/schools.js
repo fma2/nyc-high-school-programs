9bfef77e28f9589136516bb454b87cbfe1090c18
Farheen Malik <farheen.m.malik@gmail.com>
2014-12-23 11:18:51 -0500

    Modify map controls
    Move zoom control to bottom right.  Move share control to bottom right and add link to heroku page.  Move geocoder control to top right - set as kept open.  Modify map instantiation - no longer adding fma2 map as tileLayer - now in the call to L.mapbox.map with zoomControl set to false and attributionControl set to false.  Add add info control to bottom right with attribution there now.

--- app/assets/javascripts/schools.js
+++ app/assets/javascripts/schools.js
@@ -2,8 +2,21 @@
 L.mapbox.accessToken = 'pk.eyJ1IjoiZm1hMiIsImEiOiJkcmdtd0NjIn0.dw0I__cIjfXpz37Yj0DQmw';
 
 //Place map and load all markers
-var map = L.mapbox.map('map').setView([40.75, -74.09], 11).addLayer(L.mapbox.tileLayer('fma2.kgkm6i0a')).addControl(L.mapbox.shareControl({position: 'bottomright'}));
-new L.Control.Zoom({ position: 'bottomright' }).addTo(map);
+var map = L.mapbox.map('map', 'fma2.kgkm6i0a', {zoomControl:false, attributionControl: false}).setView([40.75, -74.09], 11);
+map.addControl(L.mapbox.infoControl().addInfo('<a href="https://www.mapbox.com/about/maps/" target="_blank">Maps &copy; Mapbox &copy; OpenStreetMap</a>',{position: 'bottomright'}));
+
+var zoomControl = new L.Control.Zoom({position: 'bottomright' })
+zoomControl.addTo(map);
+
+var shareControl = L.mapbox.shareControl('http://visualize-hs-nyc.herokuapp.com/', {position:'bottomright'})
+shareControl.addTo(map)
+
+//Geocoder search bar
+// Initialize the geocoder control and add it to the map.
+var geocoderControl = L.mapbox.geocoderControl('mapbox.places-v1', {
+  autocomplete: true, position: 'topright', keepOpen: true
+});
+geocoderControl.addTo(map);
 
 //Variables for items on menu
 var allSchoolsToggle = document.getElementById('listToggle')
@@ -249,12 +262,7 @@ function createMarkerList(data) {
  });
 }
 
-//Geocoder search bar
-// Initialize the geocoder control and add it to the map.
-var geocoderControl = L.mapbox.geocoderControl('mapbox.places-v1', {
-  autocomplete: true, position: 'topright'
-});
-geocoderControl.addTo(map);
+
 
 
 //Find user's location
