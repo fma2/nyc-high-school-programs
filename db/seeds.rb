a094d1de0a0d05ab22d0ed03c7e685b7475a1b89
Farheen Malik <farheen.m.malik@gmail.com>
2014-12-22 22:05:32 -0500

    Add json for high school performance data, begin to add seeding for this data to be placed in school table

--- db/seeds.rb
+++ db/seeds.rb
@@ -18,7 +18,8 @@ end
 School.destroy_all
 
 high_school_directory_2014_15 = JSON.parse(open('http://data.cityofnewyork.us/resource/n3p6-zve2.json'))
-high_school_program_data = JSON.parse(open('http://data.cityofnewyork.us/resource/mreg-rk5p.json'))
+high_school_program_data_2014_15 = JSON.parse(open('http://data.cityofnewyork.us/resource/mreg-rk5p.json'))
+high_school_performance_2014_15 = JSON.parse(open('https://data.cityofnewyork.us/resource/xahu-rkwn.json'))
 
 high_school_directory_2014_15.each do |school|
 	school = School.create(phone_number: school["phone_number"], grade_span_min: school["grade_span_min"], grade_span_max: school["grade_span_max"], primary_address_line_1: school["primary_address_line_1"], city: school["city"], state_code: school["state_code"], zip: school["zip"], school_type: school["school_type"], latitude: school["location_1"]["latitude"], longitude: school["location_1"]["longitude"], se_services: school["se_services"], total_students: school["total_students"], program_highlights: school["program_highlights"], overview_paragraph: school["overview_paragraph"], website: school["website"], extracurricular_activities: school["extracurricular_activities"], boro: school["boro"], dbn: school["dbn"], school_name: school["school_name"])
@@ -28,7 +29,13 @@ high_school_directory_2014_15.each do |school|
 	end
 end
 
-high_school_program_data.each do |program|
+# high_school_performance_2014_15.each do |school|
+# 	school = School.find_by(dbn:school["dbn"])
+# 	if school
+# 		school.update()
+# end
+
+high_school_program_data_2014_15.each do |program|
 	school = School.find_by(dbn: program["dbn"])
 	if school
 		school.programs << Program.create(program_code: program["program_code"], program_name: program["program_name"], dbn: program["dbn"], printed_school_name: program["printed_school_name"], interest_area: program["interest_area"], selection_method: program["selection_method"], selection_method_abbrevi: program["selection_method_abbreviated"], directory_page_: program["directory_page_"], borough: program["borough"], urls: program["urls"])
