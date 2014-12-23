4395dea8071977372307049f44e4abee85c0f5c1
Farheen Malik <farheen.m.malik@gmail.com>
2014-12-23 15:10:30 -0500

    Delete index method in programs controller and index route - not using anymore

--- app/controllers/programs_controller.rb
+++ app/controllers/programs_controller.rb
@@ -11,46 +11,4 @@ class ProgramsController < ApplicationController
 		end
 		gon.results_map = to_map
 	end
-
-	def index
-		@schools = School.all
-		@geojson = Array.new
-		@schools.each do |school|
-			school.programs.each do |program|
-				@geojson << {
-					type: 'Feature',
-					geometry: { 
-						type: 'Point',
-						coordinates: [school.longitude, school.latitude]
-						},
-					properties: {
-						name: school.school_name,
-						address: school.primary_address_line_1,
-						zip: school.zip,
-						boro: school.boro,
-						type: school.school_type,
-						total_students: school.total_students,
-						program_highlights: school.program_highlights,
-						overview_paragraph: school.overview_paragraph,
-						website: school.website,
-						dbn: school.dbn,
-						grade_span_min: school.grade_span_min,
-						grade_span_max: school.grade_span_max,
-						program_name: program.program_name,
-						interest_area: program.interest_area,
-						selection_method: program.selection_method,
-						selection_method_abbrevi: program.selection_method_abbrevi,
-						urls: program.urls,
-						:'marker-color' => '#00607d',
-						:'marker-symbol' => 'circle',
-						:'marker-size' => 'medium'
-					}
-				}
-			end
-		end
-		respond_to do |format|
-			format.html
-			format.json { render json: @geojson }
-		end
-	end
 end
