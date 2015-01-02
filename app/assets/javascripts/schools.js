350fc64b0a993999332c3515487f9994341c3f16
Farheen Malik <farheen.m.malik@gmail.com>
2015-01-02 11:44:50 -0500

    Modify class and ID tags for tabs in modal

--- app/assets/javascripts/schools.js
+++ app/assets/javascripts/schools.js
@@ -59,19 +59,17 @@ function addModalContent(marker) {
   '<div id="modal-map"></div>' +
   '<h3 class="title fancy">' + properties.name + '</h3>' +
   '<dl class="tabs" id="modalTabs" data-tab>' +
-  '<dd class="active"><a href="#overview"><h3>overview</h3></a></dd>' +
-  '<dd><a href="#details"><h3>details</h3></a></dd>' +
-  '<dd><a href="#programs"><h3>programs</h3></a></dd>' +
-  '<dd><a href="#performance"><h3>performance</h3></a></dd>' +
+  '<dd class="top" id="overview-tab"><a href="#overview"><h3>overview</h3></a></dd>' +
+  '<dd class="top" id="details-tab"><a href="#details"><h3>details</h3></a></dd>' +
+  '<dd class="top" id="programs-tab"><a href="#programs"><h3>special programs</h3></a></dd>' +
+  '<dd class="top" id="performance-tab"><a href="#performance"><h3>performance</h3></a></dd>' +
   '</dl>' +
   '<section class="information">' +
   '<div class="tabs-content">' +
-    '<div class="content active" id="overview">' +
-      // '<h3>overview</h3>' +
+    '<div class="content" id="overview">' +
       ''+addOverviewToModal(properties)+'' +
     '</div>' +
     '<div class="content" id="details">' +  
-      // '<h3>details</h3>' +
       ''+addDetailsToModal(properties)+'' +  //add information on schools' programs here
     '</div>' +
     '<div class="content" id="programs">' +
@@ -87,7 +85,17 @@ function addModalContent(marker) {
   $("body").append(modal);
   var modalId = '#modal' + properties.dbn;
   $(document).foundation('tab', 'reflow');
-  $(modalId).foundation('reveal', 'close')
+  // $('a.reveal-link').trigger('click');
+// $('a.close-reveal-modal').trigger('click');
+$(modalId).foundation('reveal', 'close', {})
+$(document).on('closed.fndtn.reveal', '[data-reveal]', function () {
+  // var modal = $(this);
+  // console.log(modal.context);
+   $(document).foundation('reflow');
+   console.log($('dd.top'));
+  ('dd.top').removeClass('active');
+  ('dd.content').removeClass('active');
+});
 }
 
 
@@ -96,7 +104,7 @@ function addOverviewToModal(properties) {
   '<p class="contact"><span class="phone-number">' + properties.phone + '</span>' + ' | ' +'<span class="website"><a target="_blank" href="http://' + properties.website + '">website</a></span>' +
   '<p class="grades">Grades ' + properties.grade_span_min + ' to ' + properties.grade_span_max + '</p>' +
   '<p class="type">' + properties.type + '</p>' +
-  '<p class=program-highlights>' + properties.program_highlights + '</p>'
+  '<p class=program-highlights>' + properties.program_highlights + '</p>';
   return overview
 }
 
@@ -108,7 +116,7 @@ function addDetailsToModal(properties) {
   '<p>' + properties.total_students + '</p>' +
   '<p>' + properties.overview_paragraph + '</p>' +
   '<p>' + properties.extracurricular_activities + '</p>' +
-  '<p>' + properties.se_services + '</p>' 
+  '<p>' + properties.se_services + '</p>'; 
   return details;
 }
 
@@ -118,9 +126,9 @@ function addProgramsToModal(programsData) {
   for (i=0; i< programsData.length; i++) {
     // programs << programsData[i].program_name
     programs.push('<h2 class="program-title">' + programsData[i].program_name + '</h2>' +
-    '<p class="program-interest-area">' + programsData[i].interest_area + '</p>' +
-    '<p class="program-selection-method">'+ programsData[i].selection_method + '</p>' +
-    '<p class="program-url">' + programsData[i].urls + '</p>')
+      '<p class="program-interest-area">' + programsData[i].interest_area + '</p>' +
+      '<p class="program-selection-method">'+ programsData[i].selection_method + '</p>' +
+      '<p class="program-url">' + programsData[i].urls + '</p>')
   }
   if (programs === []) {
     return'<p>No special programs</p>';
