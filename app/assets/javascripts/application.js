1cbcfe77b289dd3f9db0cdf61eb42339ca4e787f
Farheen Malik <farheen.m.malik@gmail.com>
2015-01-02 17:37:22 -0500

    Add toggle of loading class to map

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -23,11 +23,14 @@ var loader = document.getElementById('loader');
 
 startLoading();
 function startLoading() {
+  $("#map").toggleClass("loading");
   loader.className = '';
 }
-function finishedLoading() {	
+
+function finishedLoading() {
   loader.className = 'done';
   setTimeout(function() {
     loader.className = 'hide';
   }, 500);
+  $("#map").toggleClass("loading");
 }
